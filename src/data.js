37d3310ab0dcf9703a7d4fe9f42184cf763e5639
Oliver C <ocronk10@gmail.com>
2022-05-24 10:52:48 -0500

    Created view for featured products which includes placeholder img, description, price, and add to cart button. Also added header with title

---
+++ src/data.js
@@ -0,0 +1,44 @@
+const data = {
+  products: [
+    {
+      name: 'Placeholder img #1',
+      slug: 'placeholderurl1',
+      category: 'ph',
+      image: '/images/#1.png',
+      price: 120,
+      countInStock: 10,
+      brand: 'ph',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'ph description',
+    },
+
+    {
+      name: 'Placeholder img #2',
+      slug: 'placeholderurl2',
+      category: 'ph',
+      image: '/images/#2.png',
+      price: 120,
+      countInStock: 10,
+      brand: 'ph',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'ph description',
+    },
+
+    {
+      name: 'Placeholder img #3',
+      slug: 'placeholderurl3',
+      category: 'ph',
+      image: '/images/#3.png',
+      price: 120,
+      countInStock: 10,
+      brand: 'ph',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'ph description',
+    },
+  ],
+};
+
+export default data;
