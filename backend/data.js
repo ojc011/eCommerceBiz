064c8f1bcd97fdc177d417e2bbfa3c97e75fde02
Oliver C <ocronk10@gmail.com>
2022-05-25 17:43:00 -0500

    Setup nodejs server, created backend folder, created serverjs, installed express for api, moved product.js to backend folder, and created route for api products

---
+++ backend/data.js
@@ -0,0 +1,44 @@
+const data = {
+  products: [
+    {
+      name: 'Placeholder Product #1',
+      slug: 'placeholderurl1',
+      category: 'ph',
+      image: '/images/1.png',
+      price: 120,
+      countInStock: 10,
+      brand: 'ph',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'ph description',
+    },
+
+    {
+      name: 'Placeholder Product #2',
+      slug: 'placeholderurl2',
+      category: 'ph',
+      image: '/images/2.png',
+      price: 120,
+      countInStock: 10,
+      brand: 'ph',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'ph description',
+    },
+
+    {
+      name: 'Placeholder Product #3',
+      slug: 'placeholderurl3',
+      category: 'ph',
+      image: '/images/3.png',
+      price: 120,
+      countInStock: 10,
+      brand: 'ph',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'ph description',
+    },
+  ],
+};
+
+export default data;
