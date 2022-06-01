c4a31544d2f939c99e55157f93f8809d1fdaa209
Oliver C <ocronk10@gmail.com>
2022-06-01 16:23:09 -0500

    Created signin screen screen which you are redirected to once you click proceed to checkout

---
+++ frontend/src/screens/SigninScreen.js
@@ -0,0 +1,37 @@
+import { Link, useLocation } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import { Helmet } from "react-helmet-async";
+import Button from 'react-bootstrap/Button';
+
+
+export default function SigninScreen() {
+    const { search } = useLocation();
+    const redirectInUrl = new URLSearchParams(search).get('redirect');
+    const redirect = redirectInUrl ? redirectInUrl : '/';
+  return (
+    <Container className="small-container">
+      <Helmet>
+        <title>Sign in</title>
+      </Helmet>
+      <h1 className="my-3">Sign in</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" require />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" require />
+        </Form.Group>
+        <div className="mb-3">
+            <Button type="submit">Sign In</Button>
+        </div>
+        <div className="mb-3">
+            New customer?{' '}
+            <Link to={`/signup?redirect=${redirect}`}>Create your account</Link>
+        </div>
+      </Form>
+    </Container>
+  );
+}
