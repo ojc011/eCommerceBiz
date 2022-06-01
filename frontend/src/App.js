4799db2542767565542a55500fb7139da2764cfc
Oliver C <ocronk10@gmail.com>
2022-06-01 11:34:43 -0500

    In appjs added new route for cart screen, created cartscreenjs in screens folder to show list of items in cart, add and minus items, display thumbnail, show total price, remove item button, and proceed to checkout button. Also added usenavigate to product screen to redirect user to cart after adding new item to it.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Store } from './Store';
 import { useContext } from 'react';
+import CartScreen from './screens/CartScreen';
 
 
 function App() {
@@ -27,7 +28,7 @@ function App() {
                   Cart
                   {cart.cartItems.length > 0 && (
                     <Badge pill bg="danger">
-                      {cart.cartItems.reduce((a, c) => a+c.quantity, 0)}
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
                     </Badge>
                   )}
                 </Link>
@@ -39,6 +40,7 @@ function App() {
           <Container className="mt-3">
             <Routes>
               <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/cart" element={<CartScreen />} />
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
