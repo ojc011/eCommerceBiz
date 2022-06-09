b1cc7401db7d39336851f68d7a37621e612b1e77
Oliver C <ocronk10@gmail.com>
2022-06-09 12:36:53 -0500

    created two components to protect routes for logged in user, customers, and admin users. If you are not authenticated, you will be directed to the login screen.

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -8,9 +8,8 @@ const seedRouter = express.Router();
 seedRouter.get('/', async (req, res) => {
   await Product.remove({});
   const createdProducts = await Product.insertMany(data.products);
-    await User.remove({});
-    const createdUsers = await User.insertMany(data.users);
+  await User.remove({});
+  const createdUsers = await User.insertMany(data.users);
   res.send({ createdProducts, createdUsers });
 });
-
 export default seedRouter;
